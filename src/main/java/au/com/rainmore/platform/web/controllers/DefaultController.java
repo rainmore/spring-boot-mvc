eb4964e9a84c910b6658651effbf13564341717b
Jie Rong (Felix) <rainmore24@gmail.com>
2013-12-31 22:33:59 +1100

    add form example

--- src/main/java/au/com/rainmore/platform/web/controllers/DefaultController.java
+++ src/main/java/au/com/rainmore/platform/web/controllers/DefaultController.java
@@ -1,12 +1,18 @@
 package au.com.rainmore.platform.web.controllers;
 
+import au.com.rainmore.platform.web.models.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DefaultController {
+    private final Logger LOG = LoggerFactory.getLogger(DefaultController.class);
 
     @RequestMapping("/hello")
     public String hello(ModelMap modelMap, @RequestParam("name") String name) throws Exception {
@@ -25,4 +31,19 @@ public class DefaultController {
         return "hello";
     }
 
+    @RequestMapping(value = "/form")
+    public String formView(final Person person, final BindingResult bindingResult) {
+        person.setFirstName("Felix");
+        person.setLastName("Rong");
+        person.setMiddleName("Jie");
+        LOG.info(String.format("get, person: %s", person.toString()));
+        return "form";
+    }
+
+
+    @RequestMapping(value = "/form", method = RequestMethod.POST)
+    public String formProcess(final Person person, final BindingResult bindingResult) {
+        LOG.info(String.format("post, person: %s", person.toString()));
+        return "form";
+    }
 }
---
+++ src/main/java/au/com/rainmore/platform/web/models/Person.java
@@ -0,0 +1,35 @@
+package au.com.rainmore.platform.web.models;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String middleName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String toString() {
+        return String.format("[%s, %s, %s]", firstName, lastName, middleName);
+    }
+}
