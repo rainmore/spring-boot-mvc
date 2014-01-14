2558788c8b9e64f954129f79dbc3bf53011b28a1
Jie Rong (Felix) <rainmore24@gmail.com>
2014-01-14 23:09:57 +1100

    update config

--- src/main/java/net/rainmore/platform/Application.java
+++ src/main/java/net/rainmore/platform/Application.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan(basePackages = {"au.com.rainmore.platform.web"})
+@ComponentScan(basePackages = {"net.rainmore.platform"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
