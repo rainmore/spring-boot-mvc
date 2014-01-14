b5b9699a84d7184afa6f45b923fe84a36ea9b791
Jie Rong (Felix) <rainmore24@gmail.com>
2014-01-14 23:46:33 +1100

    add active and archived

--- src/main/java/net/rainmore/platform/core/models/GenericModel.java
+++ src/main/java/net/rainmore/platform/core/models/GenericModel.java
@@ -1,9 +1,6 @@
 package net.rainmore.platform.core.models;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 
 @MappedSuperclass
 public class GenericModel {
@@ -11,11 +8,41 @@ public class GenericModel {
     @GeneratedValue(strategy = GenerationType.AUTO)
     protected Long id;
 
+    @Column
+    protected Boolean active = false;
+
+    @Column
+    protected Boolean archived = false;
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public Boolean getArchived() {
+        return archived;
+    }
+
+    public void setArchived(Boolean archived) {
+        this.archived = archived;
+    }
+
+    public Boolean isActive() {
+        return active;
+    }
+
+    public Boolean isArchived() {
+        return archived;
     }
 }
